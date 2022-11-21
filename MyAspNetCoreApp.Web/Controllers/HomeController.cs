74a2d0df2af9b991832ae8f57b3fe250dfdebd12
Emre Banzaroğlu <emrebanzar@gmail.com>
2022-11-22 00:05:34 +0300

    ViewComponent added!

--- MyAspNetCoreApp.Web/Controllers/HomeController.cs
+++ MyAspNetCoreApp.Web/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using MyAspNetCoreApp.Web.Models;
+using MyAspNetCoreApp.Web.ViewModels;
 using System.Diagnostics;
 
 namespace MyAspNetCoreApp.Web.Controllers
@@ -7,20 +8,40 @@ namespace MyAspNetCoreApp.Web.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly AppDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, AppDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
         {
+            ProductListPartialViewModelMethod();
             return View();
         }
 
+        
+
         public IActionResult Privacy()
         {
+            ProductListPartialViewModelMethod();
             return View();
+        }
+        private void ProductListPartialViewModelMethod()
+        {
+            var products = _context.Product.OrderByDescending(x => x.Id).Select(x => new ProductPartialViewModel()
+            {
+                Id = x.Id,
+                Name = x.Name,
+                Price = x.Price,
+                Stock = x.Stock
+            }).ToList();
+            ViewBag.productListPartialViewModel = new ProductListPartialViewModel() //Bu sayede home index.cshtml sayfasına gönderiyoruz
+            {
+                Products = products
+            };
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
---
+++ MyAspNetCoreApp.Web/Views/Shared/ViewComponents/ProductListViewComponent.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyAspNetCoreApp.Web.Models;
+using MyAspNetCoreApp.Web.ViewModels;
+
+namespace MyAspNetCoreApp.Web.Views.Shared.ViewComponents
+{
+    public class ProductListViewComponent:ViewComponent
+    {
+        private readonly AppDbContext _context;
+
+        public ProductListViewComponent(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var viewmodels = _context.Product.Select(x => new ProductListComponentViewModel()
+            {
+                Name = x.Name,
+                Description = x.Description
+            }).ToList();
+            return View(viewmodels); //herhangi bir cshtml belirtmezsek components folder'ındaki iligli folderda cshtml dosyası arayacak ve oraya atacak.
+        }
+    }
+}
