aafe0cc5e7036c8c239f9809964a50542b55e2ca
Emre Banzaroğlu <emrebanzar@gmail.com>
2023-01-04 00:59:46 +0300

    Visitor Comment Page created with AJAX

---
+++ MyAspNetCoreApp.Web/Controllers/CookieController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyAspNetCoreApp.Web.Controllers
+{
+    public class CookieController : Controller
+    {
+        public IActionResult CookieCreate()
+        {
+            HttpContext.Response.Cookies.Append("background-color","red",new CookieOptions()
+            {
+                Expires = DateTime.Now.AddDays(2)  //cookie'nin 2 gün geçerli olmasını sağlar
+            }); //Cookie ekleme
+            return View();
+        }
+
+        public IActionResult CookieRead() 
+        {
+            var bgcolor = HttpContext.Request.Cookies["background-color"];
+            ViewBag.bgcolor = bgcolor;
+            return View();
+        }
+    }
+}
---
+++ MyAspNetCoreApp.Web/Controllers/VisitorAJAXController.cs
@@ -0,0 +1,44 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MyAspNetCoreApp.Web.Models;
+using MyAspNetCoreApp.Web.ViewModels;
+
+namespace MyAspNetCoreApp.Web.Controllers
+{
+    public class VisitorAJAXController : Controller
+    {
+        private readonly IMapper _mapper;
+        private readonly AppDbContext _context;
+
+        public VisitorAJAXController(AppDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult SaveVisitorComment(VisitorViewModel visitorViewModel)
+        {
+            var visitor = _mapper.Map<Visitors>(visitorViewModel);
+                visitor.Created = DateTime.Now;
+                _context.Visitors.Add(visitor);
+                _context.SaveChanges();
+            return Json(new { IsSuccess = "true" });
+
+        }
+
+        [HttpGet]
+        public IActionResult VisitorCommentList()
+        {
+            var visitors = _context.Visitors.OrderByDescending(x=>x.Created).ToList();
+            var visitorViewModels = _mapper.Map<List<VisitorViewModel>>(visitors);
+            return Json(visitorViewModels);
+        }
+    }
+}
